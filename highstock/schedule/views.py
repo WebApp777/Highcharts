6ca27ce05148b5d2cbfcd55ef9c9fe30b352228a
WebApp777 <killsystem78@gmail.com>
2022-04-15 20:00:58 +0300

    Поменял rangeSelector

--- highstock/schedule/views.py
+++ highstock/schedule/views.py
@@ -8,5 +8,5 @@ def index(request):
     while True:
         if time.time() - timing > 1.0:
             n = random.randint(1, 500)
-            data = {"graph": n, "graph1": 3, "graph2": 1, "graph3": 5}
+            data = {"graph": n, "graph1": 2, "graph2": 1, "graph3": 2}
             return render(request, "schedule/index.html", context=data)
